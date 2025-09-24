7fa703b6f1f1de7574b48c00738c3b2f5bbad533
shekibhadad <erentitanslayer007@gmail.com>
2025-09-24 06:45:48 -0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,172 +1,34 @@
 
 // Shekib Hadad
+//This program calculates monthly and annual interest.
 
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
-
         Scanner input = new Scanner(System.in);
 
+        System.out.print("Enter annual interest rate: ");
+        double annualInterestRate = input.nextDouble();
 
-        //1 Area of a circle
+        System.out.print("Enter number of years: ");
+        int numberOfYears = input.nextInt();
 
-        double r;
+        System.out.print("Enter loan amount: ");
+        double loanAmount = input.nextDouble();
 
-        System.out.print("Enter the Radius: ");
-        r = input.nextDouble();
+        double monthlyInterestRate = annualInterestRate / 1200;
 
+        double monthlyPayment = loanAmount * monthlyInterestRate /
+                (1 - Math.pow(1 + monthlyInterestRate, -numberOfYears * 12));
 
-        System.out.printf("The Area of Circle is: %.2f \n" , Math.PI * Math.pow( r, 2));
+        double totalPayment = monthlyPayment * numberOfYears * 12;
 
+        System.out.printf("Monthly Payment: %.2f\n", monthlyPayment);
+        System.out.printf("Total Payment: %.2f\n", totalPayment);
 
 
-        //2 Perimeter of a rectangle
-
-        double length;
-        double width;
-
-        System.out.print("Enter the Length: ");
-        length = input.nextDouble();
-
-        System.out.print("Enter the width: ");
-        width = input.nextDouble();
-
-        System.out.printf("The Perimeter of the Rectangle is: %.2f \n", 2*length + 2*width);
-
-
-
-        //3 Swap two numbers
-
-        int num1;
-        int num2;
-
-        System.out.print("Enter the First Num: ");
-        num1 = input.nextInt();
-
-        System.out.print("Enter the Second Num: ");
-        num2 = input.nextInt();
-
-        int temp = num1;
-        num1 = num2;
-        num2 = temp;
-
-        System.out.println("num1 = " + num1);
-        System.out.println("num2 = " + num2);
-
-
-
-        //4 Simple interest
-
-        double P;
-        int R;
-        int T;
-
-        System.out.print("Enter the principle: ");
-        P = input.nextDouble();
-
-        System.out.print("Enter the rate in (%): ");
-        R = input.nextInt();
-
-        System.out.print("Enter the time (in year): ");
-        T = input.nextInt();
-
-
-        System.out.println("The interest after " + T + " year is: " + (P*T*R)/100 );
-
-
-
-
-        //5 Even or odd number
-
-        int number;
-
-        System.out.print("Enter the number: ");
-        number = input.nextInt();
-
-        if (number % 2 == 0){
-            System.out.println(number + " is even.");
-        }
-        else {
-            System.out.println(number + "is odd.");
-        }
-
-
-
-
-        //6 Sum of N natural numbers
-
-        int n;
-
-
-        System.out.print("Enter the number: ");
-        n = input.nextInt();
-
-        System.out.println("The sum of " + n + " numbers is: " + n * (n + 1) / 2 );
-
-
-
-
-        //7 Celsius to fahrenheit
-
-        double C;
-
-        System.out.print("Enter the temperature in celsius: ");
-        C = input.nextDouble();
-
-        System.out.printf("The temperature in fahrenheit is:%.1f \n", (C * 9 / 5) + 32 );
-
-
-
-
-        //8 Maximum of two number
-
-        int a;
-        int b;
-
-        System.out.print("Enter the first number: ");
-        a = input.nextInt();
-
-        System.out.print("Enter the second number: ");
-        b = input.nextInt();
-
-
-        int max;
-        if (a > b){
-            System.out.println(max = a);
-        }
-        else {
-            max = b;
-        }
-
-        System.out.println("The maximum number is " + max);
-
-
-
-
-        //9 Show the leap year
-
-        int year;
-
-        System.out.print("Enter the year: ");
-        year = input.nextInt();
-
-        if (year % 4 == 0){
-            System.out.println(year + " is a leap year.");
-        }
-        else{
-            System.out.println(year + " is not a leap year.");
-        }
-
-
-
-         //10 Swap the numbers
-
-
-
-        input.close();
-
-
-        }
     }
+}
+
+
